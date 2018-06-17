3782c5f772e3f8b7678df4c4be107f81e3db47a4
Nicholas Terrell <nicholas.terrell@mnfgropup.limited>
2018-06-18 09:01:53 +1000

    removed duplicate code

--- app/middleware/body-parser.js
+++ app/middleware/body-parser.js
@@ -1,26 +1,24 @@
 const bodyParser = require('body-parser')
-const Responder = require('../lib/responder')
 
 const isEmpty = data => {
   return data.constructor === Object && Object.keys(data).length === 0
 }
 
 const defaultParser = (req, res, next) => {
-  const responder = Responder(res)
   const jsonParser = bodyParser.json({
     limit: '100kb'
   })
   jsonParser(req, res, err => {
     if (err) {
       // must be valid JSON sent with content type JSON
-      return responder.errorString('Failed to process JSON request')
+      return res.errorString('Failed to process JSON request')
     }
 
     // ensure body is not empty
     const { body = {} } = req
     if (isEmpty(body)) {
       // should always have a body
-      return responder.errorString('No request data found')
+      return res.errorString('No request data found')
     }
 
     // go to next
